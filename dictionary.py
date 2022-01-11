b06182637b44e4431201368b59f9eb11c994c8ad
Luis Eduardo <72215228+luisiasd@users.noreply.github.com>
2022-01-10 23:11:39 -0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,86 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "764732b9",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "['Triumph 1250', 87.0, 'KawasakiZ1000', 56.0, 'MT-09', '54.000']"
+      ]
+     },
+     "execution_count": 18,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# Exemple of list\n",
+    "ListMotocycle = [\"Triumph 1250\", 87.000, \"KawasakiZ1000\", 56.0000, \"MT-09\",\"54.000\"]\n",
+    "ListMotocycle"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 34,
+   "id": "493cc32a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# And that it's a dictionary.\n",
+    "Motocycle = {\"Triumph 1250\":82.000, \"kawasaki\":54.000, \"BMW x5\":186.309}\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 33,
+   "id": "5dbedfba",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Adding the new values to the dictionary.\n",
+    "Motocycle[\"Triumph 900\"]=56.000\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "40458a8d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ab145c33",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
